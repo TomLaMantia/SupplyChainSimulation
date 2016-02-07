7488af8f7840a1b567b1027f24ea7215adcbc4cc
Tom LaMantia <tom.lamantia@mail.utoronto.ca>
2016-02-07 03:48:00 -0500

    Working on the retailer class.

--- Src/Main.py
+++ Src/Main.py
@@ -1,9 +1,26 @@
 """
 -------------------------------------------------------
-The main program.
+The main program. The main program handles the simulation
+by co-ordinating the game. This involves incrementing all of the
+communication queues between the different parts of the system.
+
+The main program also acts as the customer, receiving the
+product at the end of the supply chain system.
 -------------------------------------------------------
 Author:  Tom LaMantia
 Email:   tom@lamantia.mail.utoronto.ca
 Version: February 7th 2016
 -------------------------------------------------------
 """
+
+from SupplyChainQueue import SupplyChainQueue
+from Wholesaler import Wholesaler
+from Retailer import Retailer
+ 
+# q = SupplyChainQueue()
+# q.PushOrder(10)
+# q.AdvanceQueue()
+# q.PushOrder(30)
+# print(q.TakeDelivery())
+# q.AdvanceQueue()
+# print(q.TakeDelivery())
--- Src/Retailer.py
+++ Src/Retailer.py
@@ -7,26 +7,116 @@ Email:   tom@lamantia.mail.utoronto.ca
 Version: February 7th 2016
 -------------------------------------------------------
 """
-from Wholesaler import Wholesaler
+
+from SupplyChainQueue import SupplyChainQueue
 
 class Retailer:
     
-    def __init__(self, theWholesaler, initialStock):
-        
-        self.theWholesaler = theWholesaler
+    def __init__(self, initialStock, outGoingOrdersQueue, incomingDeliveriesQueue):
+        """
+        -------------------------------------------------------
+        Constructor for the Retailer class.
+        -------------------------------------------------------
+        Preconditions: 
+            initialStock - an integer representing the current stock
+            outGoingOrdersQueue - a queue object for the outgoing orders to the wholesaler.
+                                This queue is SHARED with the wholesaler (handled by Main)!
+            incomingDeliveriesQueue - a queue object for the incoming deliveries from the wholesaler.
+                                This queue is SHARED with the wholesaler (handled by Main)!
+        Postconditions:
+            Initializes the retailer object in its initial state.
+        -------------------------------------------------------
+        """
         self.currentStock =  initialStock
+        self.numCasesOnOrderByCustomer = 0
+        self.currentBackorder = 0
+        self.outGoingOrdersQueue = outGoingOrdersQueue
+        self.incomingDeliveriesQueue = incomingDeliveriesQueue
+        self.costsIncurred = 0
         return
     
-    def RecieveCustomerOrder(self, numberOfCases):
+    def TakeTurn(self):
+        
+        #The steps for taking a turn are as follows:
+        
+        #DELIVERY IN
+        
+        #ORDER IN
+        
+        #PREPARE DELIVERY
+        
+        #PLACE ORDER
+        
+    
         
         return
     
-    def PlaceOrderToWholesaler(self, numberOfCasesToOrder):
+    def ReceiveCustomerOrder(self, numberOfCases):
+        """
+        -------------------------------------------------------
+        Receives an order from the customer
+        -------------------------------------------------------
+        Preconditions: 
+            numberOfCases - the number of cases requested by the
+                customer in a given week.
+        Postconditions:
+            Updates the current orders accordingly.
+        -------------------------------------------------------
+        """
+        self.numCasesOnOrderByCustomer += numberOfCases
+        return
+    
+    def PlaceOrderToWholesaler(self):
+        """
+        -------------------------------------------------------
+        Places an order to the wholesaler. This needs to be determined
+        by an appropriate helper function!
+        -------------------------------------------------------
+        Preconditions: 
+            None
+        Postconditions:
+            Places the order to the customer. Note: the advancement
+            of the queues is handled by the main program.
+        -------------------------------------------------------
+        """
+        self.outGoingOrdersQueue.PushOrder(5)
+        return
+    
+    def receiveOrderFromWholesaler(self):
         
+        quantityReceived = self.incomingDeliveriesQueue.TakeDelivery()
+        
+        if quantityReceived > 0:
+            self.currentStock += quantityReceived
+                
         return
     
     def DeliverBeer(self):
+        """
+        -------------------------------------------------------
+        Places an order to the wholesaler. This needs to be determined
+        by an appropriate helper function!
+        -------------------------------------------------------
+        Preconditions: 
+            None
+        Postconditions:
+            Places the order to the customer. Note: the advancement
+            of the queues is handled by the main program.
+        -------------------------------------------------------
+        """
+        deliveryQuantity = 0
         
-        return
+         #If we can fill the customer's order, we must do it.
+        if currentStock > 0 and self.currentStock >= self.numCasesOnOrderByCustomer:
+            deliveryQuantity = self.numCasesOnOrderByCustomer
+            self.currentStock -= deliveryQuantity
+            self.numCasesOnOrderByCustomer -= deliveryQuantity
+        #If the current stock cannot cover the order, we must fill as much as we can, and back-order the rest.
+        elif self.currentStock >= 0 and self.currentStock < self.numCasesOnOrderByCustomer:
+            deliveryQuantity = self.currentStock
+            self.currentStock = 0
+            self.numCasesOnOrderByCustomer -= deliveryQuantity
+
+        return deliveryQuantity
     
     
