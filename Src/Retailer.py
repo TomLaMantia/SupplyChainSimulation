0c7258e0a65f34491c2129e67b1b5cbe96fc8200
Heinz Mutzner <heinz.mutzner@swiftmail.ch>
2016-02-26 22:08:37 +0100

    Update Retailer.py
    CalcBeerTo deliver is never called in weeks 1 to 4
    deleted:
    if weekNum <= 4:
                self.PlaceOutgoingDelivery(4)
            else:

--- Src/Retailer.py
+++ Src/Retailer.py
@@ -73,10 +73,7 @@ class Retailer(SupplyChainActor):
         
         #CALCULATE AMOUNT TO BE SHIPPED, THEN SHIP IT
         #self.ShipOutgoingDeliveryToCustomer()
-        if weekNum <= 4:
-            self.customer.RecieveFromRetailer(4)
-        else:
-            self.customer.RecieveFromRetailer(self.CalcBeerToDeliver())
+        self.customer.RecieveFromRetailer(self.CalcBeerToDeliver())
         
         #PLACE ORDER TO WHOLESALER
         self.PlaceOutgoingOrder(weekNum)
